56fd1796c4db879c1cc9a52afcf90a5109e9c43b
Enubia <marcoschuster@mailbox.org>
2021-08-25 11:48:28 +0200

    project setup

---
+++ api/jest.config.ts
@@ -0,0 +1,16 @@
+const config = {
+  roots: ['<rootDir>/api/tests'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+  setupFiles: ['<rootDir>/test-setup.ts'],
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(js|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+export default config;
