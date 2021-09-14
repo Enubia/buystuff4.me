0bf9fa777f10c42004137d6b86956f9c578fc6db
Enubia <marcoschuster@mailbox.org>
2021-09-14 13:38:38 +0200

    created search components

---
+++ frontend/components/search/CategorySearch.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'CategorySearch',
+})
+export default class CategorySearch extends Vue {}
+</script>
---
+++ frontend/components/search/GeneralSearch.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'GeneralSearch',
+})
+export default class GeneralSearch extends Vue {}
+</script>
