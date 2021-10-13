6a9b8e6eb7ed307b213be720f774a6a6fd5abbbd
Enubia <marcoschuster@mailbox.org>
2021-10-13 08:22:43 +0200

    removed svg on error pages

--- frontend/components/error/404.vue
+++ frontend/components/error/404.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="gradient text-white min-h-screen flex items-center">
     <div class="container mx-auto p-4 flex flex-wrap items-center">
-      <div class="w-full md:w-5/12 text-center p-4">
-        <img src="https://themichailov.com/img/not-found.svg" alt="Not Found" />
-      </div>
-      <div class="w-full md:w-7/12 text-center md:text-left p-4">
+      <div class="w-full text-center p-4">
         <div class="text-6xl font-medium">404</div>
         <div class="text-xl md:text-3xl font-medium mb-4">
           {{ $t('404.headline') }}
